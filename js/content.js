f0e23ebc73f784e9bdbc345ee18158e34b766fb0
SchwertBoot3845 <132493191+SchwertBoot3845@users.noreply.github.com>
2024-10-28 10:17:00 -0400

    Update content.js

--- js/content.js
+++ js/content.js
@@ -5,6 +5,47 @@ import { round, score } from './score.js';
  */
 const dir = '/data';
 
+export async function fetchList() {
+    const listResult = await fetch(`${dir}/_list.json`);
+    try {
+        const list = await listResult.json();
+        return await Promise.all(
+            list.map(async (path, rank) => {
+                const levelResult = await fetch(`${dir}/${path}.json`);
+                try {
+                    const level = await levelResult.json();
+                    return [
+                        {
+                            ...level,
+                            path,
+                            records: level.records.sort(
+                                (a, b) => b.percent - a.percent,
+                            ),
+                        },
+                        null,
+                    ];
+                } catch {
+                    console.error(`Failed to load level #${rank + 1} ${path}.`);
+                    return [null, path];
+                }
+            }),
+        );
+    } catch {
+        console.error(`Failed to load list.`);
+        return null;
+    }
+}
+
+export async function fetchEditors() {
+    try {
+        const editorsResults = await fetch(`${dir}/_editors.json`);
+        const editors = await editorsResults.json();
+        return editors;
+    } catch {
+        return null;
+    }
+}
+
 export async function fetchLeaderboard() {
     const list = await fetchList();
 
@@ -20,22 +61,16 @@ export async function fetchLeaderboard() {
         const verifier = Object.keys(scoreMap).find(
             (u) => u.toLowerCase() === level.verifier.toLowerCase(),
         ) || level.verifier;
-
         scoreMap[verifier] ??= {
             verified: [],
             completed: [],
             progressed: [],
         };
-
         const { verified } = scoreMap[verifier];
-
-        // Check if verifier name is an empty string
-        const verifierScore = level.verifier === '' ? 0 : score(rank + 1, 100, level.percentToQualify);
-
         verified.push({
             rank: rank + 1,
             level: level.name,
-            score: verifierScore,
+            score: score(rank + 1, 100, level.percentToQualify),
             link: level.verification,
         });
 
@@ -44,15 +79,12 @@ export async function fetchLeaderboard() {
             const user = Object.keys(scoreMap).find(
                 (u) => u.toLowerCase() === record.user.toLowerCase(),
             ) || record.user;
-
             scoreMap[user] ??= {
                 verified: [],
                 completed: [],
                 progressed: [],
             };
-
             const { completed, progressed } = scoreMap[user];
-
             if (record.percent === 100) {
                 completed.push({
                     rank: rank + 1,
