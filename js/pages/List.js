277797428bc823bb3f2e47a3f0e829fee7cbe6c9
SchwertBoot3845 <132493191+SchwertBoot3845@users.noreply.github.com>
2024-11-30 07:48:05 -0500

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -99,20 +99,21 @@ export default {
                         </ol>
                     </template>
                     <h3>Latest Changelog</h3>
-                    <div style="font-size: 8px; line-height: 1.8;">
-                        <p style="margin-bottom: 20px;">BALLS CHALLENGE has been moved to the Verification Board.</p>
-                        <p style="margin-bottom: 20px;">BOT unnerfed ship has been moved to the Verification Board.</p>
-                        <p style="margin-bottom: 20px;">BP mini wave 2 has been moved to the Verification Board.</p>
-                        <p style="margin-bottom: 20px;">Challenge ship wave has been moved to the Verification Board.</p>
-                        <p style="margin-bottom: 20px;">Detroit Challenge X V3 has been moved to the Verification Board.</p>
-                        <p style="margin-bottom: 20px;">I said has been moved to the Verification Board.</p>
-                        <p style="margin-bottom: 20px;">JOSHUA NEVER EAR has been moved to the Verification Board.</p>
-                        <p style="margin-bottom: 20px;">KAIZO has been moved to the Verification Board.</p>
-                        <p style="margin-bottom: 20px;">LIMORAMI CHallenge has been moved to the Verification Board.</p>
-                        <p style="margin-bottom: 20px;">Marion Schneider has been moved to the Verification Board.</p>
-                        <p style="margin-bottom: 20px;">Not Cat Universe has been moved to the Verification Board.</p>
-                        <p>Oktober Chamber has been moved to the Verification Board.</p>
+                    <div style="line-height: 1.8;">
+                        <p style="font-size: 16px; margin-bottom: 20px;">BALLS CHALLENGE has been moved to the Verification Board.</p>
+                        <p style="font-size: 16px; margin-bottom: 20px;">BOT unnerfed ship has been moved to the Verification Board.</p>
+                        <p style="font-size: 16px; margin-bottom: 20px;">BP mini wave 2 has been moved to the Verification Board.</p>
+                        <p style="font-size: 16px; margin-bottom: 20px;">Challenge ship wave has been moved to the Verification Board.</p>
+                        <p style="font-size: 16px; margin-bottom: 20px;">Detroit Challenge X V3 has been moved to the Verification Board.</p>
+                        <p style="font-size: 16px; margin-bottom: 20px;">I said has been moved to the Verification Board.</p>
+                        <p style="font-size: 16px; margin-bottom: 20px;">JOSHUA NEVER EAR has been moved to the Verification Board.</p>
+                        <p style="font-size: 16px; margin-bottom: 20px;">KAIZO has been moved to the Verification Board.</p>
+                        <p style="font-size: 16px; margin-bottom: 20px;">LIMORAMI CHallenge has been moved to the Verification Board.</p>
+                        <p style="font-size: 16px; margin-bottom: 20px;">Marion Schneider has been moved to the Verification Board.</p>
+                        <p style="font-size: 16px; margin-bottom: 20px;">Not Cat Universe has been moved to the Verification Board.</p>
+                        <p style="font-size: 16px;">Oktober Chamber has been moved to the Verification Board.</p>
                     </div>
+
 
 
                     <h3>Submission Requirements</h3>
