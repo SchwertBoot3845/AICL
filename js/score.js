81561cacc25e15c902f651aaee4f9098ff012c2d
SchwertBoot3845 <132493191+SchwertBoot3845@users.noreply.github.com>
2024-12-06 12:07:36 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,9 +24,14 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
+    /*
     let score = (-24.9975*Math.pow(rank-1, 0.4) + 300) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-
+    */
+    // New Bigger Difference Formula
+    let score = (-49.995*Math.pow(rank-1, 0.4) + 400) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    
     score = Math.max(0, score);
 
     if (percent != 100) {
