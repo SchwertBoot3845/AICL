68c10a1bdc308df02ca68e5b90b630540d899799
LetzTiz <115793599+LetzTiz@users.noreply.github.com>
2025-01-07 19:34:38 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -128,7 +128,7 @@ export default {
                     </p>
                     <p>
                         Road to 5K Levels: <b>1.00%</b><br/>
-                        Road to 10K Records/Verifications: <b>2.90%</b>
+                        Road to 10K Records/Verifications: <b>3.06%</b>
                     </p>
                     <!-- 1 Level = plus 0.02% -->
                     <!-- 1 Record = plus 0.01% -->
