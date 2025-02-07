c014f3b176256839502f4a2d61108a83fb7e89cd
cheesearmy1 <cheesearmy1.0@gmail.com>
2025-02-07 09:15:14 -0800

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -124,7 +124,7 @@ export default {
                     </p>
                     <p>
                         Road to 5K Levels: <b>1.44%</b><br/>
-                        Road to 10K Records/Verifications: <b>4.01%</b>
+                        Road to 10K Records/Verifications: <b>4.05%</b>
                     </p>
                     <!-- 1 Level = plus 0.02% -->
                     <!-- 1 Record = plus 0.01% -->
