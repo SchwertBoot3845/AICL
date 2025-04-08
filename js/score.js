251a93ef39b4ee072fb42d49de481d4667d4629a
SchwertBoot3845 <132493191+SchwertBoot3845@users.noreply.github.com>
2025-04-08 19:56:44 +0200

    switch to new test formula

--- js/score.js
+++ js/score.js
@@ -24,14 +24,14 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 300) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
-    
     /*
-    // New Test Formula
-    let score = (-5(rank-1, 0.4) + 200) * (percent/100)
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
+    
+    // New Test Formula
+    let score = (-12*Math.pow(rank-1, 0.4) + 200) * ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    
     
     score = Math.max(0, score);
 
