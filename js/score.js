da9d2a7372b6f69e656574453704a4070f868255
SchwertBoot3845 <132493191+SchwertBoot3845@users.noreply.github.com>
2025-04-08 18:21:25 +0200

    score fix omg hypermarine now 300

--- js/score.js
+++ js/score.js
@@ -25,7 +25,7 @@ export function score(rank, percent, minPercent) {
     */
     // New formula
     
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 300) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     
     /*
