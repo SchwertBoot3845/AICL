5820aa4587646763a589b2feb20f6c239b8c8e19
SchwertBoot3845 <132493191+SchwertBoot3845@users.noreply.github.com>
2025-05-18 11:18:36 -0400

    one step closer O.o

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
+import Packs from './pages/Packs.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
+    { path: '/packs', component: Packs },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
