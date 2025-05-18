e5bd838760981b26ce336f3db08d2cf9aa73d455
SchwertBoot3845 <132493191+SchwertBoot3845@users.noreply.github.com>
2025-05-18 12:02:50 -0400

    now we swap

--- js/pages/Packs.js
+++ js/pages/Packs.js
@@ -1,61 +1,32 @@
+import { fetchPacks } from "../content.js";
 import { store } from "../main.js";
 import Spinner from "../components/Spinner.js";
 
-// TODO: Replace this with actual fetch logic when your packs JSON is ready
-async function fetchPacks() {
-    // Example mock data
-    return [
-        {
-            name: "Top 10 Challenges",
-            color: "#4B0082",
-            levels: ["Level A", "Level B", "Level C"],
-            points: 150,
-        },
-        {
-            name: "Speedrun Set",
-            color: "#4B0082",
-            levels: ["Speed 1", "Speed 2"],
-            points: 75,
-        },
-    ];
-}
-
 export default {
-    name: "Packs",
-    components: { Spinner },
-    template: /*html*/ `
-        <main v-if="loading">
-            <Spinner />
-        </main>
-        <main v-else class="page-packs">
+    template: `
+        <main class="page-packs">
             <div
               class="pack"
               v-for="pack in packs"
               :key="pack.id"
-              :style="{
-                borderColor: pack.color,
-                '--header-color': pack.color
-              }"
+              :style="{ borderColor: pack.color, '--header-color': pack.color }"
             >
-              <div class="pack-header">
-                <h2>{{ pack.name }}</h2>
-              </div>
-              <p>Levels:</p>
-              <ul class="pack-levels">
-                <li v-for="level in pack.levels" :key="level">{{ level }}</li>
-              </ul>
+                <div class="pack-header">
+                    <h2>{{ pack.name }}</h2>
+                </div>
+                <p>Levels:</p>
+                <ul class="pack-levels">
+                    <li v-for="level in pack.levels" :key="level">{{ level }}</li>
+                </ul>
             </div>
         </main>
     `,
     data() {
         return {
-            loading: true,
             packs: [],
-            store,
         };
     },
     async mounted() {
         this.packs = await fetchPacks();
-        this.loading = false;
     },
 };
