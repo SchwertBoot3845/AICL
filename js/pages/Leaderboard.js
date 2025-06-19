8d2afe213384ead809139366c61a6c5f804bcf54
SchwertBoot3845 <132493191+SchwertBoot3845@users.noreply.github.com>
2025-06-19 08:45:21 -0400

    might work

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -59,7 +59,7 @@ export default {
                                     </a>
                                 </td>
                                 <td class="score">
-                                    <p>+{{ localize(pack.points) }}</p>
+                                    <p>+{{ localize(pack.halfpoints) }}</p>
                                 </td>
                             </tr>
                         </table>
