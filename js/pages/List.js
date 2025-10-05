3499d3130c68ca4194a62cda73b212d6a748cec5
AliceButAgain <alicefirnewyt@gmail.com>
2025-10-05 21:31:46 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -128,7 +128,7 @@ export default {
                     <p> 
                     </p>
                     <p>
-                        Road to 5K Levels: <b>6.22%</b><br/>
+                        Road to 5K Levels: <b>6.32%</b><br/>
                         Road to 10K Records/Verifications: <b>8.53%</b>
                     </p>
                     <!-- 1 Level = plus 0.02% -->
