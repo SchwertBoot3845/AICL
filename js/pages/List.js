98c6f92dc4ebdd8ee59e40501dbf09c3f8db0a4e
SchwertBoot3845 <132493191+SchwertBoot3845@users.noreply.github.com>
2025-10-16 07:14:55 +0200

    bbibibibibiblalalala

--- js/pages/List.js
+++ js/pages/List.js
@@ -129,7 +129,7 @@ export default {
                     </p>
                     <p>
                         Road to 5K Levels: <b>6.34%</b><br/>
-                        Road to 10K Records/Verifications: <b>8.61%</b>
+                        Road to 10K Records/Verifications: <b>8.66%</b>
                     </p>
                     <!-- 1 Level = plus 0.02% -->
                     <!-- 1 Record = plus 0.01% -->
