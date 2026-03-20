2b41112ab7f320f8228d5928aff0680cc26256c4
firewyrmgd <134538391+firewyrmgd@users.noreply.github.com>
2026-03-20 15:23:24 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -129,7 +129,7 @@ export default {
                     </p>
                     <p>
                         Road to 5K Levels: <b>6.38%</b><br/>
-                        Road to 10K Records/Verifications: <b>9.05%</b>
+                        Road to 10K Records/Verifications: <b>10.91%</b>
                     </p>
                     <!-- 1 Level = plus 0.02% -->
                     <!-- 1 Record = plus 0.01% -->
